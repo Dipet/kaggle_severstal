16f2d43618e2f546523ba77077f007d4add16928
druzhinin <druzhinin@simicon.com>
2019-09-26 17:15:13 +0300

    multiscale train

---
+++ stage_experiments/resized_1_8/model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+from catalyst.contrib import registry
+
+import segmentation_models_pytorch as smp
+
+
+@registry.Model
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = smp.Unet('resnet50', encoder_weights='imagenet', classes=4, activation=None)
+
+    def forward(self, x):
+        return self.model(x)
