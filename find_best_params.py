1085fa8463d3ed16396ae67e392a7f80d4b03a75
druzhinin <druzhinin@simicon.com>
2019-10-15 15:46:45 +0300

    find best segmentation with tta

--- find_best_params.py
+++ find_best_params.py
@@ -5,49 +5,23 @@ from torch.utils.data import DataLoader
 
 
 from mlcomp.contrib.transform.albumentations import ChannelTranspose
-from mlcomp.contrib.dataset.classify import ImageDataset
 from mlcomp.contrib.transform.rle import rle2mask, mask2rle
 from mlcomp.contrib.transform.tta import TtaWrap
 
 import numpy as np
 
-import os
 from tqdm import tqdm
 
-df = read_dataset('/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/dataset/train.csv',
-                  '/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/dataset/train_images')
+from joblib import Parallel, delayed
 
-def create_transforms(additional):
-    res = list(additional)
-    # add necessary transformations
-    res.extend([
-        A.Normalize(
-            mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
-        ),
-        ChannelTranspose()
-    ])
-    res = A.Compose(res)
-    return res
 
-# Different transforms for TTA wrapper
-transforms = [
-    [],
-    [A.HorizontalFlip(p=1)],
-    [A.VerticalFlip(p=1)],
-    [A.HorizontalFlip(p=1), A.VerticalFlip(p=1)]
-]
+def get_tta_loaders(df, tta, cls, batch=16):
+    datasets = [TtaWrap(cls(df, transforms=t), tfms=t) for t
+                in tta]
+    loaders = [DataLoader(d, num_workers=6, batch_size=batch, shuffle=False) for d
+               in datasets]
 
-transforms = [create_transforms(t) for t in transforms]
-
-# datasets = [TtaWrap(BinaryClassSteelDataset(df, transforms=t), tfms=t) for t in transforms]
-# loaders = [DataLoader(d, num_workers=0, batch_size=16, shuffle=False) for d in datasets]
-
-device = 'cuda'
-
-mobilenet_cls = torch.jit.load('/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/download/12mobilenet-severstal/torchscript.pth',
-                               map_location=device)
-resnet34_cls = torch.jit.load('/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/download/resnet34-class01/torchscript.pth',
-                              map_location=device)
+    return loaders
 
 
 def predict_tta_class(models: dict, loaders_batch):
@@ -64,25 +38,24 @@ def predict_tta_class(models: dict, loaders_batch):
     for key in list(results.keys()):
         preds = torch.stack(results.pop(key))
         preds = torch.mean(preds, dim=0).squeeze()
-        results[key] = preds.detach().cpu().numpy()
+        results[key] = preds
 
     return results, data
 
 
 def find_best_multiclass(df, tta,  model, thresholds):
-    datasets = [TtaWrap(MultiClassSteelDataset(df, transforms=t), tfms=t) for t in tta]
-    loaders = [DataLoader(d, num_workers=6, batch_size=16, shuffle=False) for d in datasets]
+    loaders = get_tta_loaders(df, tta, MultiClassSteelDataset)
 
     results = {i: [] for i in thresholds}
 
     for batch in tqdm(zip(*loaders), total=len(loaders[0])):
         preds, data = predict_tta_class({'model': model}, batch)
         preds = preds['model']
-        true = data['targets'].detach().cpu().numpy()
+        true = data['targets'].cuda().long()
 
         for t in thresholds:
-            p = (preds > t).astype(int)
-            results[t].append((p == true).astype(int))
+            p = (preds > t).long()
+            results[t].append((p == true).detach().cpu().numpy().astype(int))
 
     shape = results[list(results.keys())[0]][0].shape
     accuracy = {cls: {} for cls in range(shape[1])}
@@ -102,27 +75,131 @@ def find_best_multiclass(df, tta,  model, thresholds):
     return bests
 
 
-find_best_multiclass(df, transforms, resnet34_cls, np.arange(0.05, 1.0, 0.05))
+def find_best_binary(df, tta,  model, thresholds):
+    loaders = get_tta_loaders(df, tta, BinaryClassSteelDataset)
 
-# res = []
-# without_mask = set()
-# # Iterate over all TTA loaders
-# total = len(loaders[0])
-# for loaders_batch in tqdm(zip(*loaders), total=total):
-#     preds, data = predict_tta({'mobile': mobilenet_cls,
-#                                'resnet': resnet34_cls},
-#                               loaders_batch)
-#
-#     # Batch post processing
-#     for file, p_bin, p_cls in zip(data['img_paths'], preds['mobile'], preds['resnet']):
-#         file = os.path.basename(file)
-#
-#         _p = p_cls > 0.5
-#         p_bin = (p_bin + np.max(_p)) > 0.55
-#         # Image postprocessing
-#         for i in range(4):
-#             imageid_classid = file + '_' + str(i + 1)
-#             if not p_bin or not _p[i]:
-#                 res.append({
-#                     'ImageId_ClassId': imageid_classid,
-#                     'EncodedPixels': ''})
+    results = {i: [] for i in thresholds}
+
+    for batch in tqdm(zip(*loaders), total=len(loaders[0])):
+        preds, data = predict_tta_class({'model': model}, batch)
+        preds = preds['model']
+        true = data['targets'].cuda().long()
+
+        for t in thresholds:
+            p = (preds > t).long()
+            results[t].append((p == true).detach().cpu().numpy().astype(int).flatten())
+
+    accuracy = []
+    for t, values in results.items():
+        values = np.concatenate(values, axis=0)
+        accuracy.append([t, np.sum(values) / len(values)])
+
+    accuracy = sorted(accuracy, key=lambda x: x[1])
+    print(f'Best: {accuracy[-1]} Worse: {accuracy[0]} Mean: {np.mean([i for _, i in accuracy])}')
+
+    return accuracy[-1]
+
+
+def dice(preds, true, eps=1e-7):
+    batch_size = len(true)
+    outputs = preds.view(batch_size, 4, -1)
+    targets = true.view(batch_size, 4, -1)
+
+    intersection = torch.sum(targets * outputs, dim=-1)
+    union = torch.sum(targets, dim=-1) + torch.sum(outputs, dim=-1)
+    dice = (2 * intersection / (union + eps))
+
+    return dice
+
+def find_best_mask(df, tta,  model, conf_thresholds, min_size_thresholds, batch=16):
+    loaders = get_tta_loaders(df, tta, TrainSteelDataset, batch=batch)
+
+    results = {}
+
+    for batch in tqdm(zip(*loaders), total=len(loaders[0])):
+        preds, data = predict_tta_class({'model': model}, batch)
+        preds = preds['model']
+        true = data['targets'].detach().cuda()
+        true_not_exists = true.sum(dim=(2, 3)) == 0
+
+        for conf_t in conf_thresholds:
+            _preds = (preds > conf_t).float()
+
+            for size_t in min_size_thresholds:
+                key = (conf_t, size_t)
+                if key not in results:
+                    results[key] = []
+
+                data = dice(_preds, true)
+                data = torch.where((data < 1e-7) & true_not_exists, torch.ones_like(data), data)
+                results[key].append(data)
+
+    for key in list(results.keys()):
+        results[key] = torch.cat(results[key], dim=0).mean(dim=0).cpu().numpy()
+    _results = {i: [] for i in range(4)}
+    for key, item in results.items():
+        for i in range(4):
+            _results[i].append([key, item[i]])
+
+    for cls, item in _results.items():
+        item = sorted(item, key=lambda x: x[1])
+        best = item[-1]
+        params, d = best
+        print(f'Class: {cls}; Best: conf_thres={params[0]:.2f} size_thres={params[1]:.0f} dice={d:.5f}')
+
+    # for name, data in results.items():
+    #
+    #     d = []
+    #     for key, item in data.items():
+    #         d.append([key, np.array(item).mean()])
+    #
+    #     _results[name] = sorted(d, key=lambda x: x[1])
+    #
+    # print(f'Best catalyst: {_results["catalyst"][-1]}')
+    # print(f'Best my: {_results["my"][-1]}')
+    #
+    # return {'catalyst': _results['catalyst'][-1], 'my': _results['my'][-1]}
+
+
+if __name__ == '__main__':
+    df = read_dataset(
+        '/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/dataset/train.csv',
+        '/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/dataset/train_images')
+
+
+    def create_transforms(additional):
+        res = list(additional)
+        # add necessary transformations
+        res.extend([
+            A.Normalize(
+                mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)
+            ),
+            ChannelTranspose()
+        ])
+        res = A.Compose(res)
+        return res
+
+
+    # Different transforms for TTA wrapper
+    transforms = [
+        [],
+        [A.HorizontalFlip(p=1)],
+        [A.VerticalFlip(p=1)],
+        [A.HorizontalFlip(p=1), A.VerticalFlip(p=1)]
+    ]
+
+    transforms = [create_transforms(t) for t in transforms]
+
+    device = 'cuda'
+
+
+    # print('Multiclass')
+    # model = torch.jit.load('/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/download/resnet34-class01/torchscript.pth', map_location=device)
+    # find_best_multiclass(df, transforms, model, np.arange(0.05, 1.0, 0.05))
+    #
+    # print('Binary')
+    # model = torch.jit.load('/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/download/12mobilenet-severstal/torchscript.pth', map_location=device)
+    # find_best_binary(df, transforms, model, np.arange(0.05, 1.0, 0.05))
+
+    model = torch.jit.load( '/mnt/HDD/home/druzhinin/kaggle/kaggle_severstal/download/11resnet50-hard-severstal/torchscript.pth', map_location=device)
+    find_best_mask(df, transforms, model, np.arange(0.05, 1.0, 0.05), [500, 1000, 2000, 3000])
